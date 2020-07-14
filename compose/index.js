3ba69033c3e68a7511ab4e662dcef7aca1f11e20
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-07-14 10:50:55 +0800

    add compose

--- compose/index.js
+++ compose/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
-const client = redis.createClient()
-client.set('vistis', 0)
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+})
+client.set('visits', 0)
 
 app.get('/', (req,res) => {
-  client.get('vistis', (err, vistis) =>{
+  process.exit(0)
+  client.get('visits', (err, visits) =>{
     res.send(`Number of visits is ${visits}`)
-    client.set('vistis', parseInt(vistis) + 1)
+    client.set('visits', parseInt(visits) + 1)
   });
 });
 
